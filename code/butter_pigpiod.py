8c6c50135262681614d557bd747db1fa7ed2c680
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2024-01-15 06:05:32 +0200

    add a non pigpiod version

--- code/butter_pigpiod.py
+++ code/butter_pigpiod.py
@@ -1,11 +1,13 @@
 import os
 from time import sleep
 from gpiozero import AngularServo, Robot, LED
+from gpiozero.pins.pigpio import PiGPIOFactory
 from stream import StreamingHandler, StreamingServer, output
 import picamera
 
 def play_sound(text, amp, speed):
-    command = 'espeak -a ' + str(amp) + ' -k20 -s ' + str(speed) + ' "'+text+'" 2>/dev/null'
+    command = 'espeak -a ' + amp + ' -k20 -s ' + speed + ' "'+text+'" 2>/dev/null'
+    print(command)
     os.system(command)
 
 motor_1_a_pin = 13
@@ -18,10 +20,11 @@ left_arm_servo_pin = 27
 right_arm_servo_pin = 22
 led_pin = 5
 
-neck_servo = AngularServo(neck_servo_pin, min_angle=-90, max_angle=90)
-left_arm_servo = AngularServo(left_arm_servo_pin, min_angle=-90, max_angle=90)
-right_arm_servo = AngularServo(right_arm_servo_pin, min_angle=-90, max_angle=90)
-robot = Robot(left=(motor_1_a_pin, motor_1_b_pin), right=(motor_2_a_pin, motor_2_b_pin))
+factory = PiGPIOFactory()
+neck_servo = AngularServo(neck_servo_pin, min_angle=-90, max_angle=90, pin_factory=factory)
+left_arm_servo = AngularServo(left_arm_servo_pin, min_angle=-90, max_angle=90, pin_factory=factory)
+right_arm_servo = AngularServo(right_arm_servo_pin, min_angle=-90, max_angle=90, pin_factory=factory)
+robot = Robot(left=(motor_1_a_pin, motor_1_b_pin), right=(motor_2_a_pin, motor_2_b_pin), pin_factory=factory)
 camera = picamera.PiCamera(resolution='640x480', framerate=24)
 led = LED(led_pin)
 
